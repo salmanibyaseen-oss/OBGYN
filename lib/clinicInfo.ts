5811508056596e711b4adfef32c80e854fc96ec7
salmanibyaseen-oss <salman.ib.yaseen@gmail.com>
2026-07-06 23:28:12 +0300

    Delete lib directory

--- lib/clinicInfo.ts
+++
@@ -1,7 +0,0 @@
-// بيانات العيادة العامة - مكان واحد للتعديل بدل ما تتكرر في كل صفحة
-export const CLINIC_NAME = "عيادة الدكتور أحمد يحيى الجوهري";
-export const CLINIC_TAGLINE = "متابعة رقمية · رعاية إنسانية";
-
-// TODO: غيّر الرابط ده لرابط صفحة العيادة الفعلي على فيسبوك/انستجرام
-export const CLINIC_SOCIAL_URL = "https://www.facebook.com/";
-export const CLINIC_SOCIAL_LABEL = "صفحة العيادة على فيسبوك";
